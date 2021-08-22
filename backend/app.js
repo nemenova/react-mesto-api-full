fb8af872809f3045962878bbe306fda4c034ed28
Victoria Nemenova <vikalangor@gmail.com>
2021-08-22 21:11:59 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -68,18 +68,19 @@ app.use((err, req, res, next) => {
     });
   next();
 });
-const allowedCors = [
-  'https://praktikum.tk',
-  'http://praktikum.tk',
-  'localhost:3000',
-  'http://vnemenova.nomoredomains.rocks',
-  'https://vnemenova.nomoredomains.rocks'
-];
+// const allowedCors = [
+//   'https://praktikum.tk',
+//   'http://praktikum.tk',
+//   'localhost:3000',
+//   'http://vnemenova.nomoredomains.rocks',
+//   'https://vnemenova.nomoredomains.rocks'
+// ];
 app.use(function (req, res, next) {
-  const { origin } = req.headers;
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+  // const { origin } = req.headers;
+  // if (allowedCors.includes(origin)) {
+  //   res.header('Access-Control-Allow-Origin', origin);
+  // }
+  res.header('Access-Control-Allow-Origin', "*");
   next();
 });
 app.use(function (req, res, next) {
