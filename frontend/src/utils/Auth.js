028c4cc468b64a9000ad4e79bb5aabef6369a8ad
Victoria Nemenova <vikalangor@gmail.com>
2021-08-22 16:56:06 +0300

    domain

--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://auth.nomoreparties.co';
+export const BASE_URL = 'https://nemenova.nomoredomains.rocks';
 
 function checkResponse(res) {
     if (res.status === 200 || 201) {
