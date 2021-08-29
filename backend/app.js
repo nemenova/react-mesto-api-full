122b7070cafed1b376a27dc7731d63ad38d7d707
Victoria Nemenova <vikalangor@gmail.com>
2021-08-30 00:16:02 +0300

    cors

--- backend/app.js
+++ backend/app.js
@@ -17,11 +17,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-
-app.use(cors({
-  credentials: true,
-  origin: 'https://vnemenova.nomoredomains.rocks',
-}));
+app.use(cors({ credentials: true, origin: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
