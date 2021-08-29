948824707953bd8528daf825316158a75ef64068
Victoria Nemenova <vikalangor@gmail.com>
2021-08-30 01:00:33 +0300

    func

--- backend/middlewares/centralErrorHandler.js
+++ backend/middlewares/centralErrorHandler.js
@@ -1,4 +1,4 @@
-module.exports.centralErrorHandler = (err, req, res, next) => {
+module.exports = (err, req, res, next) => {
   // если у ошибки нет статуса, выставляем 500
   const { statusCode = 500, message } = err;
 
